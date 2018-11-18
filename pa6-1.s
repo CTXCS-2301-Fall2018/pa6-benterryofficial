93237b4967837de5911e9394f8de7f1eb51fc01c
Ben <benjamin.terry@ctx.edu>
2018-11-18 07:54:40 +0000

    Completed

--- pa6-1.s
+++ pa6-1.s
@@ -1,24 +1,41 @@
-		@---------------------
-		@	Programming Assignment 6, Problem 1
-		@	Your required header information goes here
+		@	Adrian Revuelta & Ben Terry
+		@   Programming Assignment 6, Problem 1
+		@   11/14
+		@	This program will use a function that computes a^b
+		@   where a and b are integer values. We will use the pow function,
+		@   and other instructions, to compute a^b – 3*a.
 		@---------------------
 		@ In those you will use the provided function pow to comput
 		@ a^b - 3a
 		@ You must set up the arguments to pow and call it using BL
 		@ The result should be in R4 after the program completes
+
 	.extern printf
 	.global	main
-	.func main	
+	.func main
 main:	PUSH {LR}
 	@	Your code goes here
 
+    LDR R0, =a      @ LOADS A (3) TO R0
+    LDR R0, [R0]    @ LOADS CONTENTS OF R0(A) INTO R0
+    LDR R1, =b      @ LOADS B (4) IN R1
+    LDR R1, [R1]    @ LOADS CONTENTS OF R1(B) INTO R1
+    MOV R5, #3      @ MOVES 3 INTO R5
+
+    BL pow          @ BRANCH FUNCTION POW (A^B)
+
+    LDR R7, =a      @ LOADS A (4) INTO R7
+    LDR R7, [R7]    @ LOADS CONTENTS OF R7 (A) INTO R7
+
+    MUL R3, R7, R5  @ MULTIPLIES R5(3) x R7 (A) (3^a)
+    SUB R4, R0, R3  @ DOES THE SUB COMMAND = a^b-3-a AND STORES IT IN R4
 
 	@ Code to print your answer.  DO NOT CHANGE!
 	LDR	R0, =out
 	MOV	R1, R4
 	BL 	printf
 	POP	{PC}
-		
+
 	@---------------------
 	@  pow function.  DO NOT CHANGE!
 	@---------------------
